47e9bfcdca42e5b9a16a1a200defde8f63e82ea1
MinJiwon <jwjwmin111@gmail.com>
2023-11-16 15:50:56 +0900

    input 단위 props로 넘겨줌

--- src/components/allowanceInfo/ReceivingCycle.tsx
+++ src/components/allowanceInfo/ReceivingCycle.tsx
@@ -49,7 +49,7 @@ export default function ReceivingCycle(){
             (
             <CheckingButton buttonArr={buttonArr} setNotTyping={setNotTyping}/>
             ):(
-            <TypingInput />
+            <TypingInput unit={"일"}/>
             )}
             <HelpingQ>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
