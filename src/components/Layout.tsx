130a81b4291a51ccc8304afd9988b7643f9c849a
MinJiwon <jwjwmin111@gmail.com>
2023-11-17 20:59:18 +0900

    용돈 분류 선택 flow 개발 진행1

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,8 +6,10 @@ import styled from "styled-components";
 const Wrapper = styled.div`
     background-color: lemonchiffon;
     min-height: 100vh;
-    padding: calc(${({theme})=>theme.height.topbar} + ${({theme})=>theme.height.topPadding}) 1rem
-        calc(${({theme})=>theme.height.navbar} + ${({theme})=>theme.height.bottomPadding}) 1rem;
+    padding-top: calc(${({theme})=>theme.height.topbar} + ${({theme})=>theme.height.topPadding});
+    padding-left: 1rem;
+    padding-bottom: calc(${({theme})=>theme.height.navbar} + ${({theme})=>theme.height.bottomPadding});
+    padding-right: 1rem;
 `
 
 export default function Layout(){
