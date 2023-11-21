c9c16403602f47409fa79a1b3ea3afc23bea8221
MinJiwon <jwjwmin111@gmail.com>
2023-11-21 16:05:04 +0900

    카테고리 추가 페이지로 이동하는 기능 추가

--- src/components/allowanceInfo/CategoryReceiving.tsx
+++ src/components/allowanceInfo/CategoryReceiving.tsx
@@ -7,7 +7,6 @@ export default function CategoryReceiving(){
         <>
             <FormTop step={"third"} text={"용돈 금액"} title={"용돈은 주로 어디에 사용해요?"} detail={"여러 개 선택할 수 있어요."}/>
             <CategoryList />
-            {/* <AddingCategory/> */}
         </>
     )
 }
