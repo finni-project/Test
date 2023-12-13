91675c280fcedeb092a6b024597bc7bda7ba78ab
MinJiwon <jwjwmin111@gmail.com>
2023-12-13 16:09:42 +0900

    용돈 주기,금액 직접입력 페이지 UI수정, input 최소값 설정

--- src/components/TypingInput.tsx
+++ src/components/TypingInput.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
@@ -20,7 +20,7 @@ const ButtonWrapper = styled.div`
     }
     height: 3.25rem;
     padding: 0.875rem 0rem;
-    margin-bottom: 15.563rem;
+    /* margin-bottom: 15.563rem; */
     cursor: pointer;
     /* background-color: cadetblue; */
 `
@@ -63,7 +63,7 @@ const Input = styled.input`
     input[type="number"] {
         -moz-appearance: textfield;
     }
-    background-color: lightcyan;
+    /* background-color: lightcyan; */
     ${({theme})=>theme.fonts.body17b};
     color: ${({theme})=>theme.colors.neutral.n100};
 `
@@ -75,9 +75,10 @@ const DeleteBtnWrapper = styled.div`
 `
 
 const NextButton = styled.button`
-    width: 100%;
+    width: calc(100% - 2rem);
     ${({theme}) => theme.mediumBtns.primary};
-    margin-bottom: 2rem;
+    position: fixed;
+    bottom: calc(${({theme})=>theme.height.navbar} + 0.5rem);
 `
 
 type TypingInputProps = {
@@ -88,26 +89,29 @@ type TypingInputProps = {
 export default function TypingInput({nextPage, unit}:TypingInputProps){
     const [notEdited, setNotEdited] = useState<boolean>(true)
     function handleEditBtnClick(){
-        setNotEdited(false)
+        setNotEdited(!notEdited)
     }
 
-    const [cycle, setCycle] = useState<string>("")
+    const [input, setInput] = useState<string>("")
     function handleInputChange(e: React.ChangeEvent<HTMLInputElement>){
-        const { value } = e.target;
-        if(value){
-            setCycle(value);
+        const val = e.target.value;
+        const numVal = Number(val);
+        if(val && numVal > 0){
+            setInput(val);
             setBtnDisabled(false);
         } else{
-            setCycle("")
+            setInput("")
             setBtnDisabled(true);
         }
     }
     function handleIconClick(){
-        setCycle("");
+        setInput("");
         setBtnDisabled(true);
     }
 
     const [btnDisabled, setBtnDisabled] = useState<boolean>(true);
+
+    const [isToggle, setIsToggle] = useState(false);
     
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -122,7 +126,7 @@ export default function TypingInput({nextPage, unit}:TypingInputProps){
             (
             <EditWrapper>
                 <ButtonWrapper onClick={handleEditBtnClick}>
-                    <span>{unit}</span>
+                    <span>{unit==="원"?input.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","):input}{unit}</span>
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6 15.6403V17.6667C6 17.8534 6.14665 18 6.33329 18H8.35967C8.44633 18 8.53298 17.9667 8.59297 17.9L15.872 10.6277L13.3723 8.12804L6.09999 15.4004C6.03333 15.467 6 15.547 6 15.6403Z" fill="#B7B7B7"/>
                         <path d="M17.805 7.75476L16.2452 6.19497C15.9853 5.93501 15.5653 5.93501 15.3054 6.19497L14.0855 7.4148L16.5852 9.91446L17.805 8.69463C18.065 8.43466 18.065 8.01472 17.805 7.75476Z" fill="#B7B7B7"/>
@@ -130,20 +134,18 @@ export default function TypingInput({nextPage, unit}:TypingInputProps){
                 </ButtonWrapper>
             </EditWrapper>
             ):(
-            <>
-                <Form>
-                    {/* 수정가능 부분 */}
-                    <Input type="number" name="allowanceCycle" value={cycle} onChange={handleInputChange}/>
-                    <span>{unit}</span>
-                    <DeleteBtnWrapper onClick={handleIconClick}>
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 21C10.7689 21 9.60978 20.764 8.52253 20.2921C7.43528 19.8259 6.47747 19.1784 5.64909 18.3496C4.82071 17.5209 4.17066 16.5655 3.69895 15.4835C3.23298 14.3957 3 13.2331 3 11.9957C3 10.764 3.23298 9.60719 3.69895 8.52518C4.17066 7.43741 4.81783 6.47914 5.64046 5.65036C6.46884 4.82158 7.42665 4.1741 8.5139 3.70791C9.60115 3.23597 10.7603 3 11.9914 3C13.2224 3 14.3816 3.23597 15.4688 3.70791C16.5618 4.1741 17.5197 4.82158 18.3423 5.65036C19.1707 6.47914 19.8207 7.43741 20.2924 8.52518C20.7641 9.60719 21 10.764 21 11.9957C21 13.2331 20.7641 14.3957 20.2924 15.4835C19.8207 16.5655 19.1707 17.5209 18.3423 18.3496C17.5197 19.1784 16.5647 19.8259 15.4775 20.2921C14.3902 20.764 13.2311 21 12 21ZM9.21285 15.6734C9.47747 15.6734 9.69895 15.5871 9.87728 15.4144L12.0086 13.2734L14.1486 15.4144C14.3154 15.5871 14.5283 15.6734 14.7872 15.6734C15.0403 15.6734 15.2531 15.5871 15.4257 15.4144C15.5983 15.2417 15.6846 15.0288 15.6846 14.7755C15.6846 14.5281 15.5954 14.3209 15.4171 14.154L13.2685 12.0043L15.4257 9.85468C15.5983 9.67626 15.6846 9.46906 15.6846 9.23309C15.6846 8.97986 15.5983 8.76978 15.4257 8.60288C15.2589 8.43022 15.0518 8.34388 14.8044 8.34388C14.5513 8.34388 14.3384 8.43022 14.1659 8.60288L12.0086 10.7525L9.86002 8.61151C9.68169 8.43885 9.46596 8.35252 9.21285 8.35252C8.96548 8.35252 8.75551 8.43885 8.58293 8.61151C8.41611 8.77842 8.33269 8.98849 8.33269 9.24173C8.33269 9.4777 8.41898 9.68201 8.59156 9.85468L10.7488 12.0043L8.59156 14.1626C8.41898 14.3353 8.33269 14.5396 8.33269 14.7755C8.33269 15.0288 8.41611 15.2417 8.58293 15.4144C8.75551 15.5871 8.96548 15.6734 9.21285 15.6734Z" fill="#B7B7B7"/>
-                        </svg>
-                    </DeleteBtnWrapper>
-                </Form>
-                <NextButton data-disabled={btnDisabled} onClick={handleLinkClick}>다음으로</NextButton>
-            </>
+            <Form>
+                {/* 수정가능 부분 */}
+                <Input type="number" autoFocus name="allowanceCycle" value={input} onChange={handleInputChange} onBlur={handleEditBtnClick}/>
+                <span>{unit}</span>
+                <DeleteBtnWrapper onClick={handleIconClick}>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 21C10.7689 21 9.60978 20.764 8.52253 20.2921C7.43528 19.8259 6.47747 19.1784 5.64909 18.3496C4.82071 17.5209 4.17066 16.5655 3.69895 15.4835C3.23298 14.3957 3 13.2331 3 11.9957C3 10.764 3.23298 9.60719 3.69895 8.52518C4.17066 7.43741 4.81783 6.47914 5.64046 5.65036C6.46884 4.82158 7.42665 4.1741 8.5139 3.70791C9.60115 3.23597 10.7603 3 11.9914 3C13.2224 3 14.3816 3.23597 15.4688 3.70791C16.5618 4.1741 17.5197 4.82158 18.3423 5.65036C19.1707 6.47914 19.8207 7.43741 20.2924 8.52518C20.7641 9.60719 21 10.764 21 11.9957C21 13.2331 20.7641 14.3957 20.2924 15.4835C19.8207 16.5655 19.1707 17.5209 18.3423 18.3496C17.5197 19.1784 16.5647 19.8259 15.4775 20.2921C14.3902 20.764 13.2311 21 12 21ZM9.21285 15.6734C9.47747 15.6734 9.69895 15.5871 9.87728 15.4144L12.0086 13.2734L14.1486 15.4144C14.3154 15.5871 14.5283 15.6734 14.7872 15.6734C15.0403 15.6734 15.2531 15.5871 15.4257 15.4144C15.5983 15.2417 15.6846 15.0288 15.6846 14.7755C15.6846 14.5281 15.5954 14.3209 15.4171 14.154L13.2685 12.0043L15.4257 9.85468C15.5983 9.67626 15.6846 9.46906 15.6846 9.23309C15.6846 8.97986 15.5983 8.76978 15.4257 8.60288C15.2589 8.43022 15.0518 8.34388 14.8044 8.34388C14.5513 8.34388 14.3384 8.43022 14.1659 8.60288L12.0086 10.7525L9.86002 8.61151C9.68169 8.43885 9.46596 8.35252 9.21285 8.35252C8.96548 8.35252 8.75551 8.43885 8.58293 8.61151C8.41611 8.77842 8.33269 8.98849 8.33269 9.24173C8.33269 9.4777 8.41898 9.68201 8.59156 9.85468L10.7488 12.0043L8.59156 14.1626C8.41898 14.3353 8.33269 14.5396 8.33269 14.7755C8.33269 15.0288 8.41611 15.2417 8.58293 15.4144C8.75551 15.5871 8.96548 15.6734 9.21285 15.6734Z" fill="#B7B7B7"/>
+                    </svg>
+                </DeleteBtnWrapper>
+            </Form>
             )}
+            <NextButton data-disabled={btnDisabled} onClick={handleLinkClick}>다음으로</NextButton>
         </>
     )
 }
