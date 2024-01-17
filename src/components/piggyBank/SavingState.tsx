760879d89248d8ec232887e02d949db2752bb11e
MinJiwon <jwjwmin111@gmail.com>
2024-01-17 14:52:01 +0900

    저금통 페이지 경로 재설정

---
+++ src/components/piggyBank/SavingState.tsx
@@ -0,0 +1,11 @@
+import BottomSheet from "./BottomSheet";
+import SavingProgress from "./SavingProgress";
+
+export default function SavingState(){
+    return(
+        <>
+            <SavingProgress/>
+            <BottomSheet/>
+        </>
+    )
+}
--- src/pages/PiggyBank.tsx
+++ src/pages/PiggyBank.tsx
@@ -1,20 +1,11 @@
-import BottomSheet from "components/piggyBank/BottomSheet"
-import SavingProgress from "components/piggyBank/SavingProgress"
+import SavingState from "components/piggyBank/SavingState"
 import { Route, Routes } from "react-router-dom"
-import styled from "styled-components"
 
-
-const Wrapper = styled.div`
-    padding-left: 1rem;
-    padding-bottom: calc(${({theme})=>theme.height.navbar} + ${({theme})=>theme.height.bottomPadding});
-    padding-right: 1rem;
-`
 
 export default function PiggyBank(){
     return(
-        <div>
-            <SavingProgress/>
-            <BottomSheet/>
-        </div>
+        <Routes>
+            <Route path='/state' element={<SavingState/>} />
+        </Routes>
     )
 }
