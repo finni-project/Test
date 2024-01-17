38797188acffdba347c86770b60921be464ebb79
MinJiwon <jwjwmin111@gmail.com>
2024-01-17 15:41:38 +0900

    lottie 추가

--- src/components/piggyBank/GoalComplete.tsx
+++ src/components/piggyBank/GoalComplete.tsx
@@ -1,7 +1,15 @@
+import Lottie from "lottie-react";
+import clapping from "../../src_assets/clapping.json"
+import styled from "styled-components";
+
+const LottieWrapper = styled.div`
+    width: 10.813rem;
+`
+
 export default function GoalComplete(){
     return(
-        <>
-            
-        </>
+        <LottieWrapper>
+            <Lottie animationData={clapping} />
+        </LottieWrapper>
     )
 }
