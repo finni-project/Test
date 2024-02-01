86d5b67f4b6311c35facd1992f645f176d503a87
MinJiwon <jwjwmin111@gmail.com>
2024-02-01 19:01:19 +0900

    저금통 확인 페이지 size 설정

--- src/components/piggyBank/SavingState.tsx
+++ src/components/piggyBank/SavingState.tsx
@@ -1,11 +1,17 @@
+import styled from "styled-components";
 import BottomSheet from "./BottomSheet";
 import SavingProgress from "./SavingProgress";
 
+const Wrapper = styled.div`
+    width: 100%;
+    height: calc(100vh - ${({theme})=>theme.height.topbar} - ${({theme})=>theme.height.navbar});
+`
+
 export default function SavingState(){
     return(
-        <>
+        <Wrapper>
             <SavingProgress/>
             <BottomSheet/>
-        </>
+        </Wrapper>
     )
 }
